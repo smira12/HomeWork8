0ded469606fc597bc0460db04df391b08703c39e
smira12 <m1ronsa89@gmail.com>
2022-12-30 02:43:25 +0300

    Add Task №62

---
+++ Task5/Program.cs
@@ -0,0 +1,67 @@
+﻿/*Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+int GetNumber(string message)
+{
+    int result;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число!");
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 10)
+            {
+                Console.Write($"0{array[i, j]} ");
+            }
+            else
+            {
+                Console.Write(array[i, j] + " ");
+            }
+        }
+        Console.WriteLine("");
+    }
+}
+
+int size = GetNumber("Введите размерность массива: ");
+
+int[,] nums = new int[size, size];
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= size * size)
+{
+    nums[i, j] = num;
+    if (i <= j + 1 && i + j < size - 1)
+        ++j;
+    else if (i < j && i + j >= size - 1)
+        ++i;
+    else if (i >= j && i + j > size - 1)
+        --j;
+    else
+        --i;
+    ++num;
+}
+PrintArray(nums);
